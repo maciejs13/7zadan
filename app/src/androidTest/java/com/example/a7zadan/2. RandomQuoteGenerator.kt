1809e6fae7b38bb3868a974a5986f158ea4ae384
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-21 21:34:45 +0100

    Obiekt Quotes z funkcją losowania cytatu z listy oraz losowania cytatu z listy

--- app/src/androidTest/java/com/example/a7zadan/2. RandomQuoteGenerator.kt
+++ app/src/androidTest/java/com/example/a7zadan/2. RandomQuoteGenerator.kt
@@ -1,2 +1,12 @@
-package com.example.a7zadan
-
+object Quotes {
+    private val quotes = mutableListOf(
+        "Wiek nie ma znaczenia, chyba że jesteś serem.",
+        "Jeżeli czegoś nie wolno, a bardzo się chce, to można.",
+        "Inni nie mogą nam zabrać naszego szacunku dla siebie, jeśli im go nie oddamy.")
+    fun getRandomQuote(): String {
+        return quotes.random()
+    }
+    fun addQuote(quote: String){
+        quotes.add(quote)
+    }
+}
