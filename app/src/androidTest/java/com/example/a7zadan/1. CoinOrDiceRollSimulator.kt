cc90c976f7cf6f99368cb8c9392e5aa85f959163
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-21 21:27:05 +0100

    Klasa Dice z metoda roll zwracajaca Rzut kostka

--- app/src/androidTest/java/com/example/a7zadan/1. CoinOrDiceRollSimulator.kt
+++ app/src/androidTest/java/com/example/a7zadan/1. CoinOrDiceRollSimulator.kt
@@ -8,6 +8,9 @@ class Coin : coinOrDiceRoll {
     override fun roll() = arrayOf("Orzeł", "Reszka").random()
 }
 
+class Dice : coinOrDiceRoll {
+    override fun roll() = (1..6).random().toString()
+}
 
 
 fun main(){
