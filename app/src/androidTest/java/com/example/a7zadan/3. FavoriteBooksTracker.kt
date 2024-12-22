9006abd3223f753413e9da5e6a08e06271390225
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-22 19:04:52 +0100

    filtrowanie po autorze, roku wydania i sortowanie alfabetyczne po tytule

--- app/src/androidTest/java/com/example/a7zadan/3. FavoriteBooksTracker.kt
+++ app/src/androidTest/java/com/example/a7zadan/3. FavoriteBooksTracker.kt
@@ -8,6 +8,15 @@ class FavoriteBooksTracker{
 
     fun displayBooks() = books.forEach { println("${it.title}, ${it.author}, ${it.year}") }
 
+    fun filterByAuthor(author: String) = books.filter { it.author.contains(author, ignoreCase = true) }
+        .forEach { println("${it.title}, ${it.author}, ${it.year}") }
+
+    fun filterByYear(year: Int) = books.filter { it.year == year }
+        .forEach { println("${it.title}, ${it.author}, ${it.year}") }
+
+    fun sortBooksByTitle() = books.sortedBy { it.title }
+        .forEach { println("${it.title}, ${it.author}, ${it.year}") }
+
 }
 
 fun main(){
