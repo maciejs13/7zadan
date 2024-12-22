c45abaac2e7eac3e93d10bfc1601f2713c883f0e
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-22 19:31:32 +0100

    metoda do filtrowania wyniku

--- app/src/androidTest/java/com/example/a7zadan/6. Sport Results Analyzer.kt
+++ app/src/androidTest/java/com/example/a7zadan/6. Sport Results Analyzer.kt
@@ -4,4 +4,9 @@ class SportsResultsAnalyzer(private val matches: MutableList<Match>) {
     fun addMatch(score: Int) {
         matches.add(Match(score))
     }
+
+    fun filterMatches(minScore: Int): List<Match> {
+        return matches.filter { it.score >= minScore }
+    }
 }
+
