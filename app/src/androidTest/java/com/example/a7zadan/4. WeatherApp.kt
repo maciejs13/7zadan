abc9c95f1076bcaaa711774aa9d9c5aeadf2a85c
maciejs13 <maciej.sledzinski@cyfroweszkoly.pl>
2024-12-22 19:11:25 +0100

    Klasy reprezentujace rozna pogode, sloneczna, deszczowa, pochmurna

--- app/src/androidTest/java/com/example/a7zadan/4. WeatherApp.kt
+++ app/src/androidTest/java/com/example/a7zadan/4. WeatherApp.kt
@@ -2,6 +2,29 @@ interface Weather {
     fun getForecast(): String
 }
 
+class Sunny : Weather {
+    override fun getForecast(): String {
+        return "Słonecznie, idealna pogoda na spacer!"
+    }
+}
+
+class Rainy : Weather {
+    override fun getForecast(): String {
+        return "Deszczowo, weź parasol!"
+    }
+}
+
+class Cloudy : Weather {
+    override fun getForecast(): String {
+        return "Pochmurno, może padać."
+    }
+}
+
+
+
+
 fun main(){
+
+
 
 }
